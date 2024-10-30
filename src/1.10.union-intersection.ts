b4db38841ccd2653f78fb8f1b96f0566a5bfccef
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-10-31 01:06:03 +0600

    explored union and intersection types

---
+++ src/1.10.union-intersection.ts
@@ -0,0 +1,44 @@
+{
+	//
+	// union types
+
+	type FrontendDeveloper1 = "fakibazDeveloper" | "juniorDeveloper";
+	type FullstackDeveloper1 = FrontendDeveloper1 | "expertDeveloper";
+
+	type Developer = FrontendDeveloper1 | FullstackDeveloper2;
+
+	const newDeveloper: FrontendDeveloper1 = "juniorDeveloper";
+
+	type User = {
+		name: string;
+		email?: string;
+		gender: "male" | "female";
+		bloodGroup: "O+" | "A+" | "AB+";
+	};
+
+	const user1: User = {
+		name: "persian",
+		gender: "male",
+		bloodGroup: "O+",
+	};
+
+	type FrontendDeveloper2 = {
+		skills: string[];
+		designation1: "Frontend Developer";
+	};
+
+	type BackendDeveloper2 = {
+		skills: string[];
+		designation2: "Backend Developer";
+	};
+
+	type FullstackDeveloper2 = FrontendDeveloper2 & BackendDeveloper2;
+
+	const fullstackDeveloper: FullstackDeveloper2 = {
+		skills: ["HTML", "CSS", "EXPRESS"],
+		designation1: "Frontend Developer",
+		designation2: "Backend Developer",
+	};
+
+	//
+}
