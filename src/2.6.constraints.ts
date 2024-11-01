63227eed3075c59c6f1799ba595785adc74a4c5e
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-11-01 21:52:02 +0600

    explored constraints

---
+++ src/2.6.constraints.ts
@@ -0,0 +1,40 @@
+{
+	// constraints
+    type User = { id: number; name: string; email: string };
+    
+	const addCourseToStudent = <T extends User>(student: T) => {
+		const course = "Next Level Web Development";
+		return {
+			...student,
+			course,
+		};
+	};
+
+	const student3 = addCourseToStudent({
+		id: 44,
+		name: "Mr. Z",
+		email: "z@gmail.com",
+		hudai: "hudai",
+	});
+
+	const student1 = addCourseToStudent<{
+		id: number;
+		name: string;
+		email: string;
+		devType: string;
+	}>({
+		id: 222,
+		name: "Mr X",
+		email: "x@gmail.com",
+		devType: "vua",
+	});
+
+	const student2 = addCourseToStudent({
+		id: 333,
+		name: "Mr Y",
+		email: "y@gmail.com",
+		hasWatch: "Apple Watch",
+	});
+
+	//
+}
